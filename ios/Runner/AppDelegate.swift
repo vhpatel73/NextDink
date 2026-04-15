d631cf37aafd2bfbf9793dd923027324a6978c57
vhpatel73 <vhpatel73@gmail.com>
2026-04-15 18:23:49 -0400

    iOS: Configure Google Maps API and privacy permissions in native project files

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import Flutter
 import UIKit
+import GoogleMaps
 
 @main
 @objc class AppDelegate: FlutterAppDelegate, FlutterImplicitEngineDelegate {
@@ -7,6 +8,7 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyAg6tV216lYExeG2vU1bXcvf9691oTIIXE")
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 
